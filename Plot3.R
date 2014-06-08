f43688462b6b3fbd45e7a6bd3202d444ab9da7cf
manuelqt <manuel.qt@gmail.com>
2014-06-08 18:59:45 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,11 +1,11 @@
-data<-read.table("household_power_consumption.txt", sep=";", header =T, 
+dataplot<-read.table("household_power_consumption.txt", sep=";", header =T, 
                  colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'),
                  na.strings='?')
-dataplot<-data[data$Date %in% c("1/2/2007","2/2/2007"),]                 
-dataplot$datetime<-strptime(paste(dataplot$Date,dataplot$Time),"%d/%m/%Y %H:%M:%S")
+dataplotplot<-dataplot[dataplot$Date %in% c("1/2/2007","2/2/2007"),]                 
+dataplotplot$datetime<-strptime(paste(dataplotplot$Date,dataplotplot$Time),"%d/%m/%Y %H:%M:%S")
 png("plot3.png", height = 480, width = 480)
-plot(dataplot$datetime,dataplot$Sub_metering_1,xlab ="", ylab = "Energy sub metering", type ="l",col = 'black')
-lines(dataplot$datetime,dataplot$Sub_metering_2, col = "red")
-lines(dataplot$datetime,dataplot$Sub_metering_3, col = "blue")
+plot(dataplotplot$datetime,dataplotplot$Sub_metering_1,xlab ="", ylab = "Energy sub metering", type ="l",col = 'black')
+lines(dataplotplot$datetime,dataplotplot$Sub_metering_2, col = "red")
+lines(dataplotplot$datetime,dataplotplot$Sub_metering_3, col = "blue")
 legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black','red','blue'), lty = 1, lwd = 3)
 dev.off()
