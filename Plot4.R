0d108537f282c1950db35d72761a7d604d826ac9
manuelqt <manuel.qt@gmail.com>
2014-09-06 12:12:06 -0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,16 +1,39 @@
-dataplot<-read.table("household_power_consumption.txt", sep=";", header =T, colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric'),
-                  na.strings='?')
-subdataplot<-dataplot[dataplot$Date %in% c("1/2/2007","2/2/2007"),]                 
-
- subdataplot$datetime<-strptime(paste(subdataplot$Date,subdataplot$Time),"%d/%m/%Y %H:%M:%S")
-png("plot4.png", height = 480, width = 480)
- par(mfrow=c(2,2))
- 
-plot(subdataplot$datetime,subdataplot$Global_active_power,xlab ="", ylab = "Global Active Power", type ="l")
-plot(subdataplot$datetime,subdataplot$Voltage,xlab ="datetime", ylab = "Voltage", type ="l")
- plot(subdataplot$datetime,subdataplot$Sub_metering_1,xlab ="", ylab = "Energy sub metering", type ="l",col = 'black')
- lines(subdataplot$datetime,subdataplot$Sub_metering_2, col = "red")
- lines(subdataplot$datetime,subdataplot$Sub_metering_3, col = "blue")
- legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black','red','blue'), lty = 1, lwd = 3)
- plot(subdataplot$datetime,subdataplot$Global_reactive_power,xlab ="datetime", ylab = "Global_reactive_power", type ="l")
-dev.off()
+if(!file.exists("exdata-data-household_power_consumption.zip")) {
+        temp <- tempfile()
+        download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+        file <- unzip(temp)
+        unlink(temp)
+}
+power <- read.table(file, header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+plot4 <- function() {
+        par(mfrow=c(2,2))
+        
+        ##plot1
+        plot(df$timestamp,df$Global_active_power, type="l", xlab="", ylab="Global Active Power")
+        ##plot2
+        plot(df$timestamp,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+        
+        ##plot3
+        plot(df$timestamp,df$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+        lines(df$timestamp,df$Sub_metering_2,col="red")
+        lines(df$timestamp,df$Sub_metering_3,col="blue")
+        legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), bty="n", cex=.5) 
+        
+        #plot4
+        plot(df$timestamp,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+        
+        #final
+        dev.copy(png, file="plot4.png",height=480,  width=480)
+        dev.off()
+        cat("plot4.png has been saved", getwd())
+}
+plot4()
